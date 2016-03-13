508688f3882118be7632e11721d16cb52db42c2a
Utarwyn <maxime.malgorn@laposte.net>
2016-03-13 15:32:29 +0100

    Collisions with bounce & friction

--- classes/physics/Box2DPhysicEngine.js
+++ classes/physics/Box2DPhysicEngine.js
@@ -3,155 +3,95 @@
  * @class
  */
 function Box2DPhysicEngine(options){
+	if(options == null) options = {};
 	this.gameobject = null;
 
-	// Bouncing
-	this.posBeforeFall = null;
-	this.bouncing      = false;
-	this.bounceVy      = 100;
-	this.bounceAy      = 50;
+	this.mass       = options.mass || 0;
+	this.elasticity = options.elasticity || 0.6;
+	this.friction   = options.friction || 0.008;
 
-	// Quotas
-	this.maxaY = 1;
+	this.gravity    = options.gravity || 0.2;
+	this.bounce     = options.bounce || false;
 
-	// Fix position
-	this.fixedPosition = false;
-	this.lastCollision = null;
-
-	this.mass    = options.mass || 0;
-	this.reverse = options.reverse || false;
-	this.bounce  = options.bounce || false;
-
-	this.aX = 0;
-	this.aY = 0;
-
-	this.vX = 0;
-	this.vY = 0;
-
-	this.pX = 0;
-	this.pY = 0;
-
-	// Events
-	this.collidesEvents = [];
+	this.checkCollision = {left: true, right: true, up: true, down: true};
+	this.collideBounds  = new Rectangle(0, 0, 0, 0);
 }
 
 Box2DPhysicEngine.prototype = {
 
-	updateObject: function(delta){
-		// Update acceleration
-		this.aY += 0.5;
-		if(this.aY>this.maxaY) this.aY = this.maxaY;
+	setGameObject: function(gameobject){
+		this.gameobject = gameobject;
+		this.collideBounds = new Rectangle(0, 0, Game.getSize().getWidth(), Game.getSize().getHeight());
 
-		// Update Velocity
-		this.vX += this.aX * delta;
-		this.vY += this.aY * delta;
-
-		// Update position
-		if(!this.reverse){
-			this.pX += this.vX * delta * this.mass;
-			this.pY += this.vY * delta * this.mass;
-		}else{
-			this.pX -= this.vX * delta * this.mass;
-			this.pY -= this.vY * delta * this.mass;
-		}
+		console.log(this.collideBounds);
 	},
 
-	update: function(delta){
-		this.checkCollisions();
+	update: function(){
+		if(this.gameobject == null) return false;
 
-		if(this.mass==0||this.bouncing) return false;
-		if(this.gameobject==null) return false;
-		if(this.gameobject instanceof Actor && !this.gameobject.canJump) return false;
+		if(!this.checkForCollisions()){
+			// Gravity
+			this.gameobject.getVelocity().addY(this.mass * this.gravity);
+		}
 
-		if(this.posBeforeFall==null) this.posBeforeFall = this.gameobject.getCenter();
+		if(this.mass > 0) console.log(this.gameobject.getVelocity().getY());
 
-		var collision = false;
-		var sidesCollided = Game.collisionsManager.getSidesCollided(this.gameobject);
-		if(sidesCollided.length > 0) console.log(sidesCollided);
+		// Friction
+		this.gameobject.getVelocity().setX(this.gameobject.getVelocity().getX() - this.gameobject.getVelocity().getX() * this.friction);
+	},
 
-		// if(!this.reverse) collision = Game.collisionsManager.checkSideCollisions(this.gameobject, "bottom");
-		// else collision = Game.collisionsManager.checkSideCollisions(this.gameobject, "top");
-
-		if(!collision){
-			this.fixedPosition = false;
-
-			this.updateObject(delta);
-
-			this.gameobject.position.addX(this.pX);
-			this.gameobject.position.addY(this.pY);
-			this.gameobject.inAir = true;
-
-			if(this.gameobject.behavior!=null){
-				this.gameobject.behavior.lastMoveSpeed = {x: 0, y: this.pY};
+	checkForCollisions: function(){
+		// Check world collisions
+		if(this.checkCollision.up && this.gameobject.getPosition().getY() <= this.collideBounds.getTop()){
+			if(this.bounce){
+				if(this.gameobject.getVelocity().getY() < 0)
+					this.gameobject.getVelocity().setY(-this.gameobject.getVelocity().getY() * this.elasticity); 
+			}else{
+				this.gameobject.getVelocity().setY(0);
+				this.gameobject.getPosition().setY(this.collideBounds.getTop());
 			}
 
-			this.gameobject.dispatchEvent("move", {actor: this.gameobject});
-		}else{
-			if(!this.fixedPosition){
-				this.gameobject.fixPosition();
-					
-				// Bounce gameobject
-				if(this.bounce&&!this.bouncing){
-					var that = this;
-					var yToReich = this.posBeforeFall.y+this.mass;
-
-					this.bouncing = true;
-					this.bounceVy = this.mass;
-
-					var bi = setInterval(function(){
-						if(yToReich>that.gameobject.position[1]){
-							that.bouncing = false;that.reinitVars();clearInterval(bi);return false;
-						}
-
-						that.bounceAy -= 0.5;
-						if(that.bounceAy<0) that.bounceAy = 0;
-						that.bounceVy -= that.bounceAy * delta;
-
-						var pY = (that.bounceVy * delta * 2) * that.mass / yToReich * 12;
-						if(yToReich>that.gameobject.position[1]) pY /= 16;
-
-						that.gameobject.position[1] -= pY;
-
-						if(that.gameobject.onMoveEvent!=null) that.gameobject.onMoveEvent({actor: that.gameobject});
-
-						that.gameobject.dispatchEvent("move", {actor: that.gameobject});
-					}, 1);
-				}else{
-					this.reinitVars();
-				}
+			return true;
+		}
+		if(this.checkCollision.right && this.gameobject.getPosition().getX() + this.gameobject.getSize().w >= this.collideBounds.getRight()){
+			if(this.bounce){
+				if(this.gameobject.getVelocity().getX() > 0) 
+					this.gameobject.getVelocity().setX(-this.gameobject.getVelocity().getX() * this.elasticity);
+			}else{
+				this.gameobject.getVelocity().setX(0);
+				this.gameobject.getPosition().setX(this.collideBounds.getRight() - this.gameobject.getSize().w);
 			}
+
+			return true;
+		}
+		if(this.checkCollision.down && this.gameobject.getPosition().getY() + this.gameobject.getSize().h >= this.collideBounds.getBottom()){
+			if(this.bounce){
+				if(this.gameobject.getVelocity().getY() > 0) 
+					this.gameobject.getVelocity().setY(-this.gameobject.getVelocity().getY() * this.elasticity);
+			}else{
+				this.gameobject.getVelocity().setY(0);
+				this.gameobject.getPosition().setY(this.collideBounds.getBottom() - this.gameobject.getSize().h);
+			}
+
+			return true;
+		}
+		if(this.checkCollision.left && this.gameobject.getPosition().getX() <= this.collideBounds.getLeft()){
+			if(this.bounce){
+				if(this.gameobject.getVelocity().getX() < 0) 
+					this.gameobject.getVelocity().setX(-this.gameobject.getVelocity().getX() * this.elasticity);
+			}else{
+				this.gameobject.getVelocity().setX(0);
+				this.gameobject.getPosition().setX(this.collideBounds.getLeft());
+			}
+
+			return true;
 		}
 
-		if(collision&&this.lastCollision!=null&&!this.lastCollision){
-			this.fixedPosition = true;
-		}
-
-		this.lastCollision = collision;
+		return false;
 	},
-
-	checkCollisions: function(){
-		var cm = Game.collisionsManager;
-		var sidesCollided = cm.getSidesCollided(this.gameobject);
-
-		if(sidesCollided.length > 0 && this.collidesEvents.length > 0){
-			var lCo = cm.getLastObjectCollided(this.gameobject);
-
-			for(var i = 0; i < this.collidesEvents.length; i++)
-				this.collidesEvents[i](this.gameobject, lCo, sidesCollided, lCo.getPosition().clone());
-		}
-	},
-
 
 	reinitVars: function(){
-		this.aX = 0;
-		this.aY = 0;
-		this.vX = 0;
-		this.vY = 0;
-		this.pX = 0;
-		this.pY = 2;
-
-		this.gameobject.inAir = false;
-		this.posBeforeFall = this.gameobject.getCenter();
+		
 	},
 
 
