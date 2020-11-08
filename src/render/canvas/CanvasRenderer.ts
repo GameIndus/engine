2707ba18dae66dda3017ba589a55534846e551ff
development <fatapix@gmail.com>
2020-11-08 20:51:29 +0100

    Added some features class CanvasRenderer

--- src/render/canvas/CanvasRenderer.ts
+++ src/render/canvas/CanvasRenderer.ts
@@ -9,14 +9,27 @@ export default class CanvasRenderer {
 
     private _currentContext: CanvasRenderingContext2D
 
-    // private _antialias
+    private _size: Size
 
-    // private _size: Size
+    private _antialias: boolean
 
     constructor(game: Game) {
         this.game = game
 
         this._canvas = game.canvas
         this._currentContext = this._canvas.context
+        this._size = { width: 0, height: 0 }
+        this._antialias = game.config.antialias || true // change???
+
+        this.init()
     }
+
+    private init() {}
+
+    public resize(width: number, height?: number) {
+        this._size.width = width
+        this._size.height = height || width
+    }
+
+    public onResize() {}
 }
